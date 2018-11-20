1f9b22b52e8ee1600d05d4cf844b7b2b241a1e2c
de Roodenbeke Gauthier <45171246+Gauthierderoodenbeke@users.noreply.github.com>
2018-11-20 13:07:17 +0100

    Add files via upload

---
+++ fr.inti.dao/ternaire/CompositionDao.java
@@ -0,0 +1,13 @@
+package fr.inti.dao.ternaire;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import fr.inti.model.produit.Composition;
+
+@Repository
+@Qualifier(value = "compositionDao")
+public interface CompositionDao extends CrudRepository<Composition, Long> {
+
+}
---
+++ fr.inti.dao/ternaire/InteretDao.java
@@ -0,0 +1,13 @@
+package fr.inti.dao.ternaire;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import fr.inti.model.produit.Interet;
+
+@Repository
+@Qualifier(value = "interetDao")
+public interface InteretDao extends CrudRepository<Interet, Long> {
+
+}
